597fbf4b87bb68a74380c2f2a485bff498467bc0
shivamgoyal7 <goyalshivam661@gmail.com>
2020-06-15 13:11:17 +0530

    add basic layout
    Signed-off-by: shivamgoyal7 <goyalshivam661@gmail.com>

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+menuToggle.addEventListener('click', () => {
+    if (nav.className != 'active') { nav.className = 'active'; }
+    else { nav.className = ''; }
+});
